023479c8903483b3a4f79d9b69699e2e30f222b1
昨夜太平长安 <127756413+zytaipingca@users.noreply.github.com>
2023-09-10 08:49:09 +0800

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+
+int main() {
+    // 输出"Hello, World!"到屏幕
+    printf("Hello, World!\n");
+
+    return 0; // 返回0表示程序成功执行
+}
